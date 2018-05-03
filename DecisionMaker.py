a55dab2837529d2fa32fa81f250da1df5635ff03
deveringham <dceveringham@gmail.com>
2018-05-03 00:02:25 -0700

    Initial Commit

---
+++ DecisionMaker.py
@@ -0,0 +1,21 @@
+#    ___          _     _           __  ___     __          
+#   / _ \___ ____(_)__ (_)__  ___  /  |/  /__ _/ /_____ ____
+#  / // / -_) __/ (_-</ / _ \/ _ \/ /|_/ / _ `/  '_/ -_) __/
+# /____/\__/\__/_/___/_/\___/_//_/_/  /_/\_,_/_/\_\\__/_/   
+#
+# DecisionMaker.py
+# Dylan Everingham for Marissa Kohan
+#
+
+# Inputs:
+#
+# Date
+# Time
+# Weather
+# Dylan/Marissa/Both
+# Hunger Level
+# Thirst Level
+# Energy Level
+# Introversion Level
+# Stress Level
+# Mercury in Retrograde?
